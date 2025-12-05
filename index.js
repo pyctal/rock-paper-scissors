121e5ee7685cdf69cecb44032084b149c816ee04
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2025-12-05 11:26:08 +0530

    Add function to play multiple rounds
    Define function playGame to play multiple rounds of rock paper scissors
    and return the game winner. Also, debugged playRound to appropriately
    increment score and stop switch fall-through.

--- index.js
+++ index.js
@@ -24,47 +24,80 @@ function playRound(humanChoice, computerChoice) {
     case "rock":
       switch (computerChoice) {
         case "rock":
-          return "It's a tie! Rock matches rock";
+          console.log("It's a tie! Rock matches rock");
           break;
         case "paper":
           computerScore++;
-          return "You lose! Paper beats rock";
+          console.log("You lose! Paper beats rock");
           break;
         case "scissors":
           humanScore++;
-          return "You win! Rock beats scissors";
+          console.log("You win! Rock beats scissors");
           break;
       }
+      break;
 
     case "paper":
       switch (computerChoice) {
         case "rock":
           humanScore++;
-          return "You win! Paper beats rock";
+          console.log("You win! Paper beats rock");
           break;
         case "paper":
-          return "It's a tie! Paper matches paper";
+          console.log("It's a tie! Paper matches paper");
           break;
         case "scissors":
           computerScore++;
-          return "You lose! Scissors beats paper";
+          console.log("You lose! Scissors beats paper");
           break;
       }
+      break;
 
     case "scissors":
       switch (computerChoice) {
         case "rock":
-          return "You lose! Rock beats scissors";
+          computerScore++;
+          console.log("You lose! Rock beats scissors");
           break;
         case "paper":
-          return "You win! Scissors beats paper";
+          humanScore++;
+          console.log("You win! Scissors beats paper");
           break;
         case "scissors":
-          return "It's a tie! Scissors matches scissors";
+          console.log("It's a tie! Scissors matches scissors");
           break;
       }
+      break;
 
     default:
-      return "You lose! Incorrect value provided";
+      computerScore++;
+      console.log("You lose! Incorrect value provided");
   }
 }
+
+function playGame() {
+  let numberOfRounds = 5;
+  let humanSelection, computerSelection;
+
+  for (let i = 0; i < numberOfRounds; i++) {
+    humanSelection = getHumanChoice();
+    computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+    console.log(
+      "Scoreboard: Computer - " + computerScore + ", You - " + humanScore
+    );
+  }
+
+  if (humanScore > computerScore) {
+    console.log("You won this game, Game Over! Reload the page to play again.");
+  } else if (humanScore < computerScore) {
+    console.log(
+      "You lose this game, Game Over! Reload the page to play again."
+    );
+  } else {
+    console.log("It's a tie game, Game Over! Reload the page to play again.");
+  }
+}
+
+playGame();
